743e5e3b38476b4228734f123a977dd2a4cae58e
haha096 <lucky-96@naver.com>
2026-03-18 17:44:45 +0900

    3주차_수행과제_commit_2

--- 1-3_3week/main.py
+++ 1-3_3week/main.py
@@ -7,9 +7,9 @@ import os
 # 새로운 파일 혹은 디렉토리를 작성하는 것도 가능
 
 
-# -----------------------------------------
+# -----------------------------------------------
 # Mars_Base_Inventory_List.csv 파일을 읽어 출력
-# -----------------------------------------
+# -----------------------------------------------
 
 # os.path는 폴더명이나 파일명의 존재유무를 파악할 수 있는 모듈
 # os.path.abspath -> 파일의 절대경로를 찾아주는 모듈
@@ -34,5 +34,45 @@ with open(csv_file_path, 'r', encoding='utf-8') as c_file_path:
     # <_csv.reader object at 0x02351WCEW7GW254>
     mars_list = list(reader)
 
-print(mars_list)
+# print(mars_list)
 
+
+# -----------------------------------------------
+# 배열 내용을 인화성 높은 순으로 정렬
+# 인화성 지수가 0.7이상 되는 목록을 뽑아 별도로 출력
+# -----------------------------------------------
+
+data_list = mars_list[1:]
+
+def get_flammability(x):
+    return float(x[4])
+
+flammability_sorted2 = sorted(data_list, key=get_flammability, reverse=True)
+
+flammability_sorted = sorted(data_list, key=lambda x : float(x[4]), reverse=True)
+
+# 인화성물질이 높은 순으로 정렬된 리스트
+# print(flammability_sorted)
+
+danger_flammability_list = []
+
+for i in flammability_sorted :
+    if(float(i[4]) >= 0.7) :
+        danger_flammability_list.append(i)
+
+print(danger_flammability_list)
+
+# -----------------------------------------------
+# 인화성 지수가 0.7 이상되는 목록을 csv 포멧 & 저장
+# Mars_Base_Inventory_danger.csv로 저장한다
+# -----------------------------------------------
+
+danger_flammability_csv_file_path = os.path.join(current_dir, 'Mars_Base_Inventory_danger.csv')
+
+
+danger_flammability_file = open(danger_flammability_csv_file_path, 'w', encoding='utf-8', newline='')
+
+writer = csv.writer(danger_flammability_file)
+writer.writerows(danger_flammability_list)
+
+danger_flammability_file.close()
